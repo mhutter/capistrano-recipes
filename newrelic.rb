8a1b263c1bf934f802e111b21649b53c3cb035b6
mhutter <manuel.hutter@gmail.com>
2012-05-18 15:37:58 +0200

    actually the `use_...` stuff is not needed for newrelic... simply comment out the load statement in the `deploy.rb` file

--- newrelic.rb
+++ newrelic.rb
@@ -1,20 +1,15 @@
 set_default(:newrelic_key, false)
 set_default(:newrelic_config) { "#{shared_path}/config/newrelic.yml" }
-set_default(:use_newrelic) { !!newrelic_key }
 
 namespace :newrelic do
   desc "Set up New Relic RPM Config for this App"
   task :setup, roles: :web do
-    if use_newrelic
-      template "newrelic.yml.erb", newrelic_config
-    end
+    template "newrelic.yml.erb", newrelic_config
   end
   after "deploy:setup", "newrelic:setup"
   
   task :create_symlink, roles: :web do
-    if use_newrelic
-      run "ln -nfs #{newrelic_config} #{release_path}/config/newrelic.yml"
-    end
+    run "ln -nfs #{newrelic_config} #{release_path}/config/newrelic.yml"
   end
   after "deploy:create_symlink", "newrelic:create_symlink"
 end
