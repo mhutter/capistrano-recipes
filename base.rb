6edcb2b2eefb8258058c2e509c35f910575e0b77
Manuel Hutter <manuel.hutter@gmail.com>
2012-05-21 15:22:27 +0200

    postgresql now adds the ppa:pitti/postgresql repo for backports on older Ubuntu versions.

--- base.rb
+++ base.rb
@@ -1,3 +1,4 @@
+# Helper Methods
 def template(from, to)
   erb = File.read(File.expand_path("../templates/#{from}", __FILE__))
   put ERB.new(erb).result(binding), to
@@ -7,6 +8,10 @@ def set_default(name, *args, &block)
   set(name, *args, &block) unless exists?(name)
 end
 
+# More defaults
+set_default(:lsb_release) { capture "lsb_release -r | cut -f 2" }
+
+# Cap Tasks
 namespace :deploy do
   desc "Set Up the server"
   task :install do
