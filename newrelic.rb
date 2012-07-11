9cf83c1e83e843e1108d16ea93e1cf4043ddbc9f
Manuel Hutter <manuel.hutter@gmail.com>
2012-07-11 17:42:49 +0200

    Implemented installation of newrelic-sysmond.
    closes #1.

--- newrelic.rb
+++ newrelic.rb
@@ -1,10 +1,19 @@
 set_default(:newrelic_key, false)
 set_default(:newrelic_config) { "#{shared_path}/config/newrelic.yml" }
+set_default(:newrelic_sysmond, false)
 
 namespace :newrelic do
   desc "Set up New Relic RPM Config for this App"
   task :setup, roles: :web do
     template "newrelic.yml.erb", newrelic_config if newrelic_key
+    if newrelic_sysmond
+      run "#{sudo} wget -q -O /etc/apt/sources.list.d/newrelic.list http://download.newrelic.com/debian/newrelic.list"
+      run "#{sudo} apt-key adv --keyserver hkp://subkeys.pgp.net --recv-keys 548C16BF"
+      run "#{sudo} apt-get -qq update"
+      run "#{sudo} apt-get -yq install newrelic-sysmond"
+      run "#{sudo} nrsysmond-config --set license_key=#{newrelic_key}"
+      run "#{sudo} service newrelic-sysmond start"
+    end
   end
   after "deploy:setup", "newrelic:setup"
   
