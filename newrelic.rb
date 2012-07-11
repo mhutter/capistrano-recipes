96e66fd20bf7788852660e4f4081e8180745700e
Manuel Hutter <manuel.hutter@gmail.com>
2012-07-11 17:13:35 +0200

    Only do stuff if `newrelic_key` is provided
    closes #2.

--- newrelic.rb
+++ newrelic.rb
@@ -4,12 +4,12 @@ set_default(:newrelic_config) { "#{shared_path}/config/newrelic.yml" }
 namespace :newrelic do
   desc "Set up New Relic RPM Config for this App"
   task :setup, roles: :web do
-    template "newrelic.yml.erb", newrelic_config
+    template "newrelic.yml.erb", newrelic_config if newrelic_key
   end
   after "deploy:setup", "newrelic:setup"
   
   task :create_symlink, roles: :web do
-    run "ln -nfs #{newrelic_config} #{release_path}/config/newrelic.yml"
+    run "ln -nfs #{newrelic_config} #{release_path}/config/newrelic.yml" if newrelic_key
   end
   after "deploy:create_symlink", "newrelic:create_symlink"
 end
