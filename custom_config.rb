6090cdc8c0d1b47019bf3c9a19df8b23048e3db1
Manuel Hutter <manuel.hutter@gmail.com>
2012-08-30 21:24:28 +0200

    symlink custom config earlier

--- custom_config.rb
+++ custom_config.rb
@@ -4,5 +4,5 @@ namespace :custom_config do
   task :create_symlink, roles: :app do
     run "ln -nfs #{shared_path}/config/custom.yml #{release_path}/config/custom.yml"
   end
-  after "deploy:create_symlink", "custom_config:create_symlink"
+  after "deploy:finalize_update", "custom_config:create_symlink"
 end
