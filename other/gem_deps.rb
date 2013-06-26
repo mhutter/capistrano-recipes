164bde69e6f8b56a08d64befbfd77dc9e720873c
Leon Miller-Out <leon@singlebrook.com>
2013-06-26 13:20:55 -0400

    Clean up gem_deps install

--- other/gem_deps.rb
+++ other/gem_deps.rb
@@ -3,8 +3,7 @@ namespace :gemdeps do
   desc "Install dependencies for compiling various gems"
 
   task :install, :roles => :app do
-    packages = %w(libxslt-dev libxml2-dev) # For Nokogiri
-    sudo "apt-get -yqq install #{packages.join(' ')}"
+    apt_install 'libxslt-dev libxml2-dev' # For Nokogiri
   end
 
   before 'bundle:install', 'gemdeps:install'
