f4b4f3b4c9559de12d98376ae6728e78efc344c3
Leon Miller-Out <leon@singlebrook.com>
2013-09-19 13:17:09 -0400

    Disable default site and set server name

--- webserver/apache2.rb
+++ webserver/apache2.rb
@@ -3,6 +3,15 @@ namespace :apache2 do
   desc "Install apache2 from package manager"
   task :install, :roles => :web do
     apt_install 'apache2'
+
+    # Disable the default site
+    sudo 'a2dissite default'
+
+    put_as_root %Q( # Prevent warning about "Could not reliably determine the server's fully qualified domain name?"
+                    ServerName localhost
+      ).gsub(/^ +/, ''),
+      '/etc/apache2/sites-available/default-name'
+    sudo 'a2ensite default-name'
   end
   after "deploy:install", "apache2:install"
 
