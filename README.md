3121c97af9d8a2a6478ed09cf01dff15a9b42ec6
Leon Miller-Out <leon@singlebrook.com>
2013-09-19 12:32:39 -0400

    Use config/install/recipes instead of config/deploy/recipes

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Setting up new server(s)
 
 1. If capistrano will be used for app deployment, add `capistrano` and `capistrano-ext` to the :development section of your Gemfile and run `bundle install`. Otherwise, run `gem install capistrano capistrano-ext` if you're just using capistrano for server setup.
 2. Run `capify .` to bootstrap your application for Capistrano.
-3. `git submodule add -b ubuntu12.04.2 git@github.com:singlebrook/capistrano-recipes.git config/deploy/recipes`
+3. `git submodule add -b ubuntu12.04.2 git@github.com:singlebrook/capistrano-recipes.git config/install/recipes`
 4. `git submodule update --init`
 5. Create yourself a sudoer user on each server:
   * `adduser [you] --ingroup sudo`
@@ -29,13 +29,13 @@ Setting up new server(s)
 7. `sudo chmod 440 /etc/sudoers.d/passwordless`
 8. If you're not already using capistrano for deployment, copy the example `config/deploy.rb`:
 
-    cp -r config/deploy/recipes/example/deploy.rb config/deploy.rb
+    cp -r config/install/recipes/example/deploy.rb config/deploy.rb
 
   If you are using capistrano already, review `example/deploy.rb` and make sure you've got all of the necessary settings. You'll definitely need the `load 'config/install.rb'` line.
 
 9. Copy the example recipe selection file:
 
-    cp -r config/deploy/recipes/example/deploy/install.rb config/install.rb
+    cp -r config/install/recipes/example/deploy/install.rb config/install.rb
 
   and modify it as necessary for your desired configuration.
 
--- example/deploy/install.rb
+++ example/deploy/install.rb
@@ -1,21 +1,21 @@
 # Server install tasks. Run with:
 # cap [level] deploy:install -s user=[sudoer_user]
 # sudoer_user must have passwordless sudo available.
-load 'config/deploy/recipes/base'
-load 'config/deploy/recipes/security/firewall'
-load 'config/deploy/recipes/security/fail2ban'
-load 'config/deploy/recipes/security/deploy_user'
-load 'config/deploy/recipes/scm/git'
-load 'config/deploy/recipes/webserver/apache2'
-load 'config/deploy/recipes/database/mysql'
-load 'config/deploy/recipes/database/postgresql'
-load 'config/deploy/recipes/other/memcached'
-load 'config/deploy/recipes/log/logrotate_rails'
-load 'config/deploy/recipes/mail/postfix'
+load 'config/install/recipes/base'
+load 'config/install/recipes/security/firewall'
+load 'config/install/recipes/security/fail2ban'
+load 'config/install/recipes/security/deploy_user'
+load 'config/install/recipes/scm/git'
+load 'config/install/recipes/webserver/apache2'
+load 'config/install/recipes/database/mysql'
+load 'config/install/recipes/database/postgresql'
+load 'config/install/recipes/other/memcached'
+load 'config/install/recipes/log/logrotate_rails'
+load 'config/install/recipes/mail/postfix'
 
 set :ruby_version, 'ruby1.8'
-load 'config/deploy/recipes/language/ruby'
+load 'config/install/recipes/language/ruby'
 
 # Things that depend on Ruby should go down here
-load 'config/deploy/recipes/appserver/passenger'
-load 'config/deploy/recipes/other/gem_deps'
+load 'config/install/recipes/appserver/passenger'
+load 'config/install/recipes/other/gem_deps'
